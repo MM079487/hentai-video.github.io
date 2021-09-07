d4b186838e56dfcd068c995f9306939d0a50043c
MM87 <71704916+MM879487@users.noreply.github.com>
2021-09-07 10:21:19 +0800

    Add files via upload

--- show.js
+++ show.js
@@ -8,3 +8,12 @@ audio.play();
 function show_hide2(){
     document.getElementById("image2").style.display="inline";
 }
+
+function showPage() {
+    document.getElementById("loader").style.display="none";
+    document.getElementById("myDiv").style.display="block";
+}
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
